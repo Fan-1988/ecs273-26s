f4c2aecc1ee65d78ce371f9b5b2849fb67ce3638
Fan-1988 <sijiafan6@gmail.com>
2026-04-29 14:46:28 -0700

    Finish homework 3

--- Homework3/dqfan/src/App.tsx
+++ Homework3/dqfan/src/App.tsx
@@ -1,16 +1,26 @@
 
-import { BarChart } from "./component/example";
-import RenderOptions from "./component/options";
+import { useState } from "react";
+import LineChart from "./component/LineChart";
+import NewsList from "./component/NewsList";
+import TSNEScatter from "./component/TSNEScatter";
+import RenderOptions, { STOCKS } from "./component/options";
 // A "extends" B means A inherits the properties and methods from B.
 
 
 export default function App() {
+  const [selectedStock, setSelectedStock] = useState(STOCKS[0]);
+
   return (
     <div className="flex flex-col h-full w-full">
       <header className="bg-zinc-400 text-white p-2 flex flex-row align-center">
         <h2 className="text-left text-2xl">Homework 3</h2>
-        <label htmlFor="bar-select" className="mx-2">Select a category:
-          <select id = 'bar-select' className="bg-white text-black p-2 rounded mx-2">
+        <label htmlFor="stock-select" className="mx-2">Select a stock:
+          <select
+            id="stock-select"
+            className="bg-white text-black p-2 rounded mx-2"
+            value={selectedStock}
+            onChange={(event) => setSelectedStock(event.target.value)}
+          >
               <RenderOptions />
           </select>
         </label>
@@ -19,23 +29,23 @@ export default function App() {
         <div className="flex flex-col w-2/3">
 
           <div className="h-1/4 p-2">
-            <h3 className="text-left text-xl">View 1 to be replaced by the view title</h3>
-            <div className="border-2 border-gray-300 rounded-xl">
-              <BarChart />
+            <h3 className="text-left text-xl">Stock Overview Line Chart</h3>
+            <div className="border-2 border-gray-300 rounded-xl h-[calc(100%_-_2rem)]">
+              <LineChart selectedStock={selectedStock} />
             </div>
           </div>
           <div className="h-3/4 p-2">
-            <h3 className="text-left text-xl h-[2rem]">View 2 to be replaced by the view title</h3>
+            <h3 className="text-left text-xl h-[2rem]">T-SNE Scatter Plot</h3>
             <div className="border-2 border-gray-300 rounded-xl h-[calc(100%_-_2rem)]">
-              <p className="text-center text-gray-500 mt-20">Empty View 2</p>
+              <TSNEScatter selectedStock={selectedStock} />
             </div>
           </div>
           
         </div>
         <div className="w-1/3 h-full p-2">
-            <h3 className="text-left text-xl h-[2rem]">View 3 to be replaced by the view title</h3>
+            <h3 className="text-left text-xl h-[2rem]">List of News</h3>
             <div className="border-2 border-gray-300 rounded-xl h-[calc(100%_-_2rem)]">
-              <p className="text-center text-gray-500 mt-20">Empty View 3</p>
+              <NewsList selectedStock={selectedStock} />
             </div>
           </div>
         
--- Homework3/dqfan/src/component/options.tsx
+++ Homework3/dqfan/src/component/options.tsx
@@ -1,18 +1,31 @@
-import Data from "../../data/demo.json";
-
-import { Bar } from "../types";
-
-// A "extends" B means A inherits the properties and methods from B.
-interface CategoricalBar extends Bar{
-    category: string;
-}
+export const STOCKS = [
+  "AAPL",
+  "BAC",
+  "CAT",
+  "CVX",
+  "DAL",
+  "GOOGL",
+  "GS",
+  "HAL",
+  "JNJ",
+  "JPM",
+  "KO",
+  "MCD",
+  "META",
+  "MMM",
+  "MSFT",
+  "NKE",
+  "NVDA",
+  "PFE",
+  "UNH",
+  "XOM",
+];
 
 
 export default function RenderOptions() {
-  const bars: CategoricalBar[] = Data.data;
-  return bars.map((bar, index) => (
-    <option key={index} value={bar.category}>
-      {bar.category}
+  return STOCKS.map((stock) => (
+    <option key={stock} value={stock}>
+      {stock}
     </option>
   ));
-  }
+}
